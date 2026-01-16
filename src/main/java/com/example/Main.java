c897661a104540d63e8b5bdae993445351798ba0
adela.serbulescu <adela@mail.com>
2026-01-16 12:36:18 +0200

    Created requested project.

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+class Main {
+    public static void main(String[] args) {
+       Roata r1 = new Roata(1);
+       Roata r2 = new Roata(2);
+       Roata r3 = new Roata(3);
+       Roata r4 = new Roata(4);
+       
+       Masina m = new Masina("Audi");
+       m.adaugaRoata(r1);
+       m.adaugaRoata(r2);
+       m.adaugaRoata(r3);
+       m.adaugaRoata(r4);
+       
+       m.calculeazaArie();
+    }
+ }  
